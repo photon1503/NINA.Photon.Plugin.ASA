7e7ff193842861e39ef56222d2c4ebfb15da2a74
photon1503 <g.hitz@gmx.at>
2023-11-03 18:35:59 +0100

    NINA 3 manifest

---
+++ NINA.Photon.Plugin.ASA/Deploy/createManifest.cmd
@@ -0,0 +1,9 @@
+pwsh.exe
+
+.\CreateNET7Manifest.ps1 `
+    -createArchive `
+    -includeAll `
+    -appendVersionToArchive  `
+    -file "C:\Users\Gerald\AppData\Local\NINA\Plugins\3.0.0\ASA Tools\NINA.Photon.Plugin.ASA.dll" `
+    -installerUrl https://github.com/photon1503/NINA.Photon.Plugin.ASA/releases/download/3.0.0.0/NINA.Photon.Plugin.ASA.3.0.0.0.zip 
+                  
--- NINA.Photon.Plugin.ASA/postbuild.bat
+++ NINA.Photon.Plugin.ASA/postbuild.bat
@@ -1,6 +1,3 @@
-echo %1
-echo %2
-echo %3
 if not exist "%localappdata%\NINA\Plugins" (
         mkdir  "%localappdata%\NINA\Plugins"
 )
@@ -9,6 +6,7 @@ if not exist "%localappdata%\NINA\Plugin\ASA Tools" (
 )
 
 xcopy C:\git\NINA.Photon.Plugin.ASA\NINA.Photon.Plugin.ASA\bin\Release\net7.0-windows\NINA.Photon.Plugin.ASA.dll "%localappdata%\NINA\Plugins\ASA Tools" /h/k/r/y
+
 xcopy C:\git\NINA.Photon.Plugin.ASA\NINA.Photon.Plugin.ASA\bin\Release\net7.0-windows\NINA.Photon.Plugin.ASA.pdb "%localappdata%\NINA\Plugins\ASA Tools" /h/k/r/y
      
-xcopy C:\Users\Gerald\.nuget\packages\antlr4.runtime.standard\4.11.1\Antlr4.Runtime.Standard.dll "%localappdata%\NINA\Plugins\ASA Tools" /h/k/r/y      
+xcopy C:\Users\Gerald\.nuget\packages\antlr4.runtime.standard\4.11.1\lib\net45\Antlr4.Runtime.Standard.dll "%localappdata%\NINA\Plugins\ASA Tools" /h/k/r/y      
